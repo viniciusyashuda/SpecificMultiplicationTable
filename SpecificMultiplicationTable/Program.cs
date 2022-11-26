ffdab97b8d433c9e2614eed88fa6747ebd4da218
shuda <viniciusyashuda008@gmail.com>
2022-11-26 18:31:37 -0300

    Add project files.

---
+++ SpecificMultiplicationTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SpecificMultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, result;
+
+            Console.Write("Enter any number: ");
+            number = int.Parse(Console.ReadLine());
+
+            for (int multiplier = 0; multiplier <= 10; multiplier++)
+            {
+                result = number * multiplier;
+                Console.WriteLine(number + " * " + multiplier + " = " + result);
+            }
+        }
+    }
+}
